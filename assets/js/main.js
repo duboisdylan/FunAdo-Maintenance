57704d1d9fa3d17b043054ae9afa9c0fb5a03c7c
DUBOIS Dylan <dubois.dylan@yahoo.fr>
2021-04-25 22:31:53 +0200

    Update main.js for delete old copyright

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,7 +81,7 @@
 		}
 		// AJAX MailChimp
 		$(".newsletter-form").ajaxChimp({
-			url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
+			url: "https://xx.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
 			callback: callbackFunction
 		});
 		
